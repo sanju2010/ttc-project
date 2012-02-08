00d5429d9015ba05608b5dd26adc4870d810766a
jerome.rocheteau <jerome.rocheteau@46808198-70a3-22bd-050a-ffcc9506992b>
2012-02-08 10:08:03 +0000

    metrics updated
    git-svn-id: http://ttc-project.googlecode.com/svn@198 46808198-70a3-22bd-050a-ffcc9506992b

--- trunk/sources/eu/project/ttc/metrics/EditDistance.java
+++ trunk/sources/eu/project/ttc/metrics/EditDistance.java
@@ -2,6 +2,8 @@ package eu.project.ttc.metrics;
 
 public interface EditDistance {
 
+	public double normalize(int distance, String source, String target);
+	
 	public int compute(String source, String target);
 	
 }
