a5cd0504c14e56284dfd5b4c52711b7d6b026a58
sebastian <sebastian@dictanova.com>
2014-02-18 10:58:20 +0100

    Fully moved to 1.5 version. Updated code compatibility.

--- trunk/ttc-term-suite/sources/eu/project/ttc/tools/commons/TermSuiteVersion.java
+++ trunk/ttc-term-suite/sources/eu/project/ttc/tools/commons/TermSuiteVersion.java
@@ -29,7 +29,7 @@ import java.io.File;
 public class TermSuiteVersion {
 
     /** Current version of the program */
-    public static final String VERSION = "1.5-SNAPSHOT";
+    public static final String VERSION = "1.5";
     /** Title */
     public static final String TITLE = "Term Suite";
     /** Summary description of TermSuite */
